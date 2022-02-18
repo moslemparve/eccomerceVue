840c6dd77f4eb4b90ef40e91ca80e807258a0125
moslem parve <muslimparvi@gmail.com>
2022-02-18 17:28:06 +0500

    first commit

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::view('/{path?}', 'welcome');
 Route::view('/{path?}/{id?}', 'welcome');
